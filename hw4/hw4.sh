10a166cc30b76c136ec0761d4976a1cdd0d50973
KeHongYing <b06902074@ntu.edu.tw>
2019-04-12 12:00:32 +0800

    4/12 second commit

--- hw4/hw4.sh
+++ hw4/hw4.sh
@@ -1,5 +1,6 @@
 #! /usr/env/bin bash
 
+wget https://www.dropbox.com/s/ffszulsbxkikrbq/CNN_model_18.h5?dl=1 -O CNN_model_18.h5
 python load_train.py $1
 python plot.py CNN_model_18.h5 X_train.npy Y_train.npy X_lime_train.npy $2
 
